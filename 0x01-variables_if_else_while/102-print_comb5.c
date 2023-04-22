4e80eda399f5882dfea3ab9c09056509cd328b3b
Cielo32 <asmae.sbiti@gmail.com>
2023-04-21 17:02:26 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,9 +7,9 @@ int main(void)
 {
 	int i, n;
 
-	for (i = 0; i <= 99; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (n = i; n <= 99; n++)
+		for (n = i + 1; n < 100; n++)
 		{
 			putchar(i / 10 + '0');
 			putchar(i % 10 + '0');
@@ -17,7 +17,7 @@ int main(void)
 			putchar(n / 10 + '0');
 			putchar(n % 10 + '0');
 
-			if (i != 99 || n != 99)
+			if (i != 98 || n != 99)
 			{
 				putchar(',');
 				putchar(' ');
