e8fd12ef9d55579f8b07fe84b27b4a96631c6110
Cielo32 <asmae.sbiti@gmail.com>
2023-04-26 08:53:02 -0700

    the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,7 +2,7 @@
 /**
  * main - fibonacci <3
  * Purpose - no hardcode
- * Return:  (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
@@ -14,26 +14,29 @@ int main(void)
 	unsigned long int bef2;
 	unsigned long int aft1;
 	unsigned long int aft2;
+
 	printf("%lu", bef);
 
 	for (i = 1; i < 91; i++)
 	{
-		printf(",%lu", aft);
+		printf(", %lu", aft);
 		aft += bef;
 		bef = aft - bef;
 	}
+
 	bef1 = (bef / l);
 	bef2 = (bef % l);
 	aft1 = (aft / l);
 	aft2 = (aft % l);
-	for (i = 92; i < 99; i++)
-	{ 
-		printf("%lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % l);
+
+	for (i = 91; i < 99; ++i)
+	{
+		printf(", %lu", aft1 + (aft2 / l));
+		printf(", %lu", aft2 % l);
 		aft1 = aft1 + bef1;
 		bef1 = aft1 - bef1;
 		aft2 = aft2 + bef2;
-		bef2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
 	}
 	printf("\n");
 	return (0);
