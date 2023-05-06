a478c72a5529815f2cab99a6fefb87735700224f
Cielo32 <asmae.sbiti@gmail.com>
2023-05-06 09:05:39 -0700

    Again

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *str)
 
 	while (str[i])
 	{
-		while (!(str[i] >= 'a' && str[i] >= 'z'))
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
 		if (i == 0 || str[i - 1] == ' '|| str[i - 1] == '\t'
 				|| str[i - 1] == '\n'
