7ffd7d2cde0ca929e03ee0ec2deda080c3e8adb3
Cielo32 <asmae.sbiti@gmail.com>
2023-05-05 17:45:11 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,17 +6,31 @@
  * @str: The string
  * Return: A pointer to the changed string
  */
-char *cap_string(char *)
+char *cap_string(char *str)
 {
 	int i = 0;
 
 	while (str[i])
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
+		while (!(str[i] >= 'a' && str[i] >= 'z'))
 			i++;
-		if (str[i - 1] == ' ' ||
-				str[i - 1] == '\t' ||
-				str[i - 1] == '\n' ||
-				str[i - 1] == ',' ||
-				str[i - 1] == '.' |
-				|
+		if (i == 0 || str[i - 1] == ' '|| str[i - 1] == '\t'
+				|| str[i - 1] == '\n'
+				|| str[i - 1] == ','
+				|| str[i - 1] == ';'
+				|| str[i - 1] == '.'
+				|| str[i - 1] == '!'
+				|| str[i - 1] == '?'
+				|| str[i - 1] == '"'
+				|| str[i - 1] == '('
+				|| str[i - 1] == ')'
+				|| str[i - 1] == '{'
+				|| str[i - 1] == '}')
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}
