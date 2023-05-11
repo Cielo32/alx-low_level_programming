169adb74b8b73a3a24e02e7dd1428548be87dc4d
Cielo32 <asmae.sbiti@gmail.com>
2023-05-10 18:27:19 -0700

    Inception. Is it possible?

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number - A function that returns 1 if the input integer is a prime number
+ * otherwise return 0
+ * @n: Number to value
+ * Return: integer value
+ */
+int is_prime_number(int n)
+{
+
