7d5b72268fef1a680009518473b2bb3c67e8e432
Cielo32 <asmae.sbiti@gmail.com>
2023-05-16 15:30:25 -0700

    I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,67 +1,71 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 
 /**
- * count_words - Helper function to count the number of words in a string
- * @str: String to eavaluate
+ * count_word - Helper function to count the number of words in a string
+ * @s: String to eavaluate
  * Return: number of words
  */
-int count_words(char *str)
+int count_word(char *s)
 {
 	int count = 0;
 	int word = 0;
+	int flag = 0;
 
-	while (*str)
+	for (count = 0; s[count] != '\0'; count++)
 	{
-		if (*str == ' ')
+		if (s[count] == ' ')
+			flag = 0;
+		else if (flag == 0)
 		{
-			word = 0;
+			flag = 1;
+			word++;
 		}
-		else if (!word)
-		{
-			word = 1;
-			count++;
-		}
-		str++;
 	}
-	return (count);
+	return (word);
 }
 /**
  * strtow - Write a function that splits a string into words
- * @str: string
+ * @str: string to split
  * Return: NULL if str == NULL or str == ""
  * if function fails, it should return NULL
  */
 char **strtow(char *str)
 {
-	int words, index, t;
-	char *a;
-	char **is;
+	char **m, *t;
+	int i, k = 0, l = 0, w, c = 0, s, e;
 
-	if (str == NULL || *str == '\0')
+	while (*(str + l))
+		l++;
+	w = count_word(str);
+	if (w == 0)
 		return (NULL);
-	words = count_words(str);
-	is = malloc(sizeof(char *) * (words + 1));
-	if (is == NULL)
+	m = (char **) malloc(sizeof(char *) * (w + 1));
+		if (m == NULL)
 		return (NULL);
-	index = 0;
-	a = strtok(str, " ");
-	while (a != NULL)
+	for (i = 0; i <= l; i++)
 	{
-		is[index] = strdup(a);
-		if (is[index] == NULL)
+		if (str[i] == ' ' || str[i] == '\0')
 		{
-			for (t = 0; t < index; t++)
-				free(is[t]);
-			free(is);
-			return (NULL);
+			if (c)
+			{
+				e = i;
+				t = (char *)malloc(sizeof(char) * (c + 1));
+				if (t == NULL)
+					return (NULL);
+				while (s < e)
+					*t++ = str[s++];
+				*t = '\0';
+				m[k] = t - c;
+				k++;
+				c = 0;
+			}
 		}
-		index++;
-		a = strtok(NULL, " ");
+		else if (c++ == 0)
+			s = i;
 	}
-	is[index] = NULL;
-	return (is);
+	m[k] = NULL;
+	return (m);
 }
