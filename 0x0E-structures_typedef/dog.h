18b10317e5b77f29b29d39ad11951450e550bc73
Cielo32 <asmae.sbiti@gmail.com>
2023-05-22 16:33:13 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A dog basic info
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
